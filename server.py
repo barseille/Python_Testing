6ebbb0ac5f08150326d3682cd2fec7457fb63772
BarseillePonn <ponn.barseille@gmail.com>
2023-08-04 03:51:42 +0200

    Commit before merge

--- server.py
+++ server.py
@@ -1,14 +1,16 @@
 import json
-from flask import Flask,render_template,request,redirect,flash,url_for
+from flask import Flask, render_template, request, redirect, flash, url_for
 
 
 def loadClubs():
+    """Charge et renvoie la liste des clubs à partir du fichier 'clubs.json'."""
     with open('clubs.json') as c:
          listOfClubs = json.load(c)['clubs']
          return listOfClubs
 
 
 def loadCompetitions():
+    """Charge et renvoie la liste des compétitions à partir du fichier 'competitions.json'."""
     with open('competitions.json') as comps:
          listOfCompetitions = json.load(comps)['competitions']
          return listOfCompetitions
@@ -22,16 +24,54 @@ clubs = loadClubs()
 
 @app.route('/')
 def index():
+    """Retourne la page d'index."""
     return render_template('index.html')
+
+
+class EmailError(Exception):
+    """Exception levée pour les erreurs dans le module club."""
+    def __init__(self, email, message="n'a pas été trouvé !"):
+        self.email = email
+        self.message = message
+        super().__init__(self.message)
+
+    def __str__(self):
+        return f"{self.email} {self.message}"
+
+
+def get_email(email):
+    """Récupère le club à partir de l'email."""
+    if not email:
+        raise ValueError("L'email ne peut pas être une chaîne vide.")
+    for club in clubs:
+        if club['email'] == email:
+            return club
+    raise EmailError(email)
+
+
+
 
 @app.route('/showSummary',methods=['POST'])
 def showSummary():
-    club = [club for club in clubs if club['email'] == request.form['email']][0]
-    return render_template('welcome.html',club=club,competitions=competitions)
+    """
+    Traite la requête POST de la page d'index.
+    Trouve le club à partir de l'email dans les données du formulaire 
+    et rend la page de bienvenue pour ce club.
+    """
+    try:
+        club = get_email(request.form['email'])
+        return render_template('welcome.html',club=club,competitions=competitions)
+    except EmailError as e:
+        flash(str(e))
+        return redirect(url_for('index'))
 
 
 @app.route('/book/<competition>/<club>')
 def book(competition,club):
+    """
+    Gère la réservation d'une compétition.
+    Trouve le club et la compétition à partir des paramètres de l'URL et rend la page de réservation.
+    """
     foundClub = [c for c in clubs if c['name'] == club][0]
     foundCompetition = [c for c in competitions if c['name'] == competition][0]
     if foundClub and foundCompetition:
@@ -43,6 +83,10 @@ def book(competition,club):
 
 @app.route('/purchasePlaces',methods=['POST'])
 def purchasePlaces():
+    """
+    Gère l'achat de places dans une compétition.
+    Met à jour le nombre de places dans la compétition et affiche un message de succès.
+    """
     competition = [c for c in competitions if c['name'] == request.form['competition']][0]
     club = [c for c in clubs if c['name'] == request.form['club']][0]
     placesRequired = int(request.form['places'])
@@ -51,9 +95,7 @@ def purchasePlaces():
     return render_template('welcome.html', club=club, competitions=competitions)
 
 
-# TODO: Add route for points display
-
-
 @app.route('/logout')
 def logout():
+    """Déconnecte l'utilisateur et redirige vers la page d'index."""
     return redirect(url_for('index'))
