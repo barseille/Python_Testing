d4a201508131d7fb557d5416ee67b4aeea6c4532
BarseillePonn <ponn.barseille@gmail.com>
2023-08-15 13:44:29 +0200

    correction test_booking

--- tests/unit/test_booking.py
+++ tests/unit/test_booking.py
@@ -1,9 +1,68 @@
+# def test_book_existing_competition_and_club(client, mock_clubs, mock_competitions):
+#     """Teste la réservation pour une compétition et un club existants."""
+    
+#     # Arrange
+#     competition_name = mock_competitions[0]['name']  
+#     club_name = mock_clubs[0]['name']  
+    
+#     # Act
+#     result = client.get(f"/book/{competition_name}/{club_name}")
+
+#     # Assert
+#     assert result.status_code == 200
+#     assert competition_name in result.data.decode()
+
+
+# def test_book_future_competition(client, mock_clubs, mock_competitions):
+#     """Teste la réservation pour une compétition future."""
+    
+#     # Arrange
+#     competition_name = mock_competitions[0]['name']  
+#     club_name = mock_clubs[0]['name']  
+    
+#     # Act
+#     result = client.get(f"/book/{competition_name}/{club_name}")
+
+#     # Assert
+#     assert result.status_code == 200
+#     assert competition_name in result.data.decode()
+
+
+# def test_book_non_existent_competition(client, mock_clubs):
+#     """Teste la réservation pour une compétition inexistante."""
+    
+#     # Arrange
+#     competition_name = "NonExistentCompetition"
+#     club_name = mock_clubs[0]['name']  
+#     # Act
+#     result = client.get(f"/book/{competition_name}/{club_name}")
+
+#     # Assert
+#     assert result.status_code == 200
+#     assert "Erreur - veuillez réessayer" in result.data.decode()
+
+
+# def test_book_non_existent_club(client, mock_clubs, mock_competitions):
+#     """Teste la réservation pour un club inexistant."""
+    
+#     # Arrange
+#     competition_name = mock_competitions[0]['name']  
+#     club_name = "NonExistentClub"
+
+#     # Act
+#     result = client.get(f"/book/{competition_name}/{club_name}")
+
+#     # Assert
+#     assert result.status_code == 200
+#     assert "Erreur - veuillez réessayer" in result.data.decode()
+
+
 def test_book_existing_competition_and_club(client, mock_clubs, mock_competitions):
     """Teste la réservation pour une compétition et un club existants."""
     
     # Arrange
-    competition_name = mock_competitions[0]['name']  
-    club_name = mock_clubs[0]['name']  
+    competition_name = mock_competitions[0]['name']
+    club_name = mock_clubs[0]['name']
     
     # Act
     result = client.get(f"/book/{competition_name}/{club_name}")
@@ -17,8 +76,8 @@ def test_book_future_competition(client, mock_clubs, mock_competitions):
     """Teste la réservation pour une compétition future."""
     
     # Arrange
-    competition_name = mock_competitions[0]['name']  
-    club_name = mock_clubs[0]['name']  
+    competition_name = mock_competitions[0]['name']
+    club_name = mock_clubs[0]['name']
     
     # Act
     result = client.get(f"/book/{competition_name}/{club_name}")
@@ -33,27 +92,26 @@ def test_book_non_existent_competition(client, mock_clubs):
     
     # Arrange
     competition_name = "NonExistentCompetition"
-    club_name = mock_clubs[0]['name']  
+    club_name = mock_clubs[0]['name']
+    
     # Act
     result = client.get(f"/book/{competition_name}/{club_name}")
 
     # Assert
-    assert result.status_code == 200
-    assert "Erreur - veuillez réessayer" in result.data.decode()
+    assert result.status_code == 404
+    assert "Erreur - Club ou compétition non trouvés, veuillez réessayer" in result.data.decode()
 
 
 def test_book_non_existent_club(client, mock_clubs, mock_competitions):
     """Teste la réservation pour un club inexistant."""
     
     # Arrange
-    competition_name = mock_competitions[0]['name']  
+    competition_name = mock_competitions[0]['name']
     club_name = "NonExistentClub"
 
     # Act
     result = client.get(f"/book/{competition_name}/{club_name}")
 
     # Assert
-    assert result.status_code == 200
-    assert "Erreur - veuillez réessayer" in result.data.decode()
-
-
+    assert result.status_code == 404
+    assert "Erreur - Club ou compétition non trouvés, veuillez réessayer" in result.data.decode()
